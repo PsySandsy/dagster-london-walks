901e975f43651f9768323b086bcd05448300d548
PsySandsy <12517406+PsySandsy@users.noreply.github.com>
2025-07-17 12:04:38 +0100

    af

--- dagster_london_walks/assets.py
+++ dagster_london_walks/assets.py
@@ -286,7 +286,6 @@ def file_from_s3(s3: S3Resource) -> MaterializeResult:
 
     return MaterializeResult(
         metadata={
-            "Number of London Walks": MetadataValue.int(data.section_name.unique().to_markdown()),
             "Number of Sections": MetadataValue.int(data.section_name.count()),
             "Number of Sections per Walk": MetadataValue.md(data.section_number.value_counts().to_markdown()),
             "Preview of DataFrame": MetadataValue.md(data.head().to_md())
