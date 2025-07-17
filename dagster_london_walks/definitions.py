bbc6e94b342bef20deed6ac9cd216868dcfe4ce5
PsySandsy <12517406+PsySandsy@users.noreply.github.com>
2025-07-17 10:18:21 +0100

    fix: needed to define S3 as a resource in definitions

--- dagster_london_walks/definitions.py
+++ dagster_london_walks/definitions.py
@@ -1,9 +1,10 @@
 from dagster import Definitions, load_assets_from_modules
+from dagster_aws.s3 import S3Resource
 
 from dagster_london_walks import assets  # noqa: TID252
 
 all_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
-    assets=all_assets
+    assets=all_assets, resources={"s3": S3Resource(region_name="eu-west-2")}
 )
